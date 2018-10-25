362021667e4835537a11677afea220e593a9251a
Juan <jlq2.1997@gmail.com>
2018-10-25 15:26:07 +0200

    add enem type

--- src/enemy.h.s
+++ src/enemy.h.s
@@ -4,18 +4,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enemy
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.macro DefineEnemy _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct  
+.macro DefineEnemy _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct, _type
 
     DefineEntity _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile
     .db  _direct        ;; Sentido, 0 => left
                         ;;          1 => right
+    .db  _type
                         
 .endm
 
 
-.macro DefineEnemyShoot _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct, _k_max_num_obs, _k_obs_size, _m_num_obs, _m_next_obs, _m_alive_obs, _m_murieron_obs, _suf
+.macro DefineEnemyShoot _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct,_type, _k_max_num_obs, _k_obs_size, _m_num_obs, _m_next_obs, _m_alive_obs, _m_murieron_obs, _suf
 
-    DefineEnemy _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct  
+    DefineEnemy _name, _x, _y,_oldx, _oldy _vx, _vy, _w, _h, _sprite, _upd, _tile, _direct, _type
 
     .db _k_max_num_obs   ;; Maximo de objetos
     .db _k_obs_size      ;; Obstacle size (in Bytes)
@@ -30,13 +31,14 @@
 
 e_size = 14
 e_direct           = 0 + e_size
-enm_k_max_num_obs  = 1 + e_size
-enm_k_obs_size     = 2 + e_size ;; posible constante
-enm_m_num_obs      = 3 + e_size
-enm_m_next_obs     = 4 + e_size
-enm_m_alive_obs    = 6 + e_size 
-enm_m_murieron_obs = 7 + e_size 
-enm_shot_array     = 8 + e_size
+e_type             = 1 + e_size
+enm_k_max_num_obs  = 2 + e_size
+enm_k_obs_size     = 3 + e_size ;; posible constante
+enm_m_num_obs      = 4 + e_size
+enm_m_next_obs     = 5 + e_size
+enm_m_alive_obs    = 7 + e_size 
+enm_m_murieron_obs = 8 + e_size 
+enm_shot_array     = 9 + e_size
 
 
 
