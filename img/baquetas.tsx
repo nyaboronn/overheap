b03e5f8d6a5e21f7c4709965f07d4b71e85e6fa5
Juan <jlq2.1997@gmail.com>
2018-10-31 19:50:39 +0100

    remove old stuff

--- img/baquetas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="baquetas" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../Imágenes/baquetas.png" width="124" height="118"/>
-</tileset>
--- img/ss_b20f323e1d27e4573a2afae3f4db9021da89dd3c.1920x1080.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ss_b20f323e1d27e4573a2afae3f4db9021da89dd3c.1920x1080" tilewidth="4" tileheight="4" tilecount="57600" columns="320">
- <image source="../../../../../ss_b20f323e1d27e4573a2afae3f4db9021da89dd3c.1920x1080.jpg" width="1280" height="720"/>
-</tileset>
